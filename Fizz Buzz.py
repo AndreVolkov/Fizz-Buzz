cfde712fa2503bd8d285b953b22c655458ac21d3
Ande <a.volkov@lajm.lt>
2023-02-11 15:52:11 +0200

    The function is made separate from the program code

--- Fizz Buzz.py
+++ Fizz Buzz.py
@@ -1,15 +1,17 @@
 # Fizz Buzz
 
 def FizzBuzz(n):
-    for i in range(1, n+1):
-        if i % 3 == 0 and i % 5 == 0:
-            print('Fizz Buzz')
-        elif i % 3 == 0:
-            print('Fizz')
-        elif i % 5 == 0:
-            print('Buzz')
-        else:
-            print(i)
+    if n % 3 == 0 and n % 5 == 0:
+        print('Fizz Buzz')
+    elif n % 3 == 0:
+        print('Fizz')
+    elif n % 5 == 0:
+        print('Buzz')
+    else:
+        print(n)
+    return FizzBuzz
         
-FizzBuzz(int(input('Введите n:')))
+n = int(input('Введите n:'))
 
+for i in range(1, n+1):
+    FizzBuzz(i)
